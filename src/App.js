3c08b8228f100fdf9668d1f13414100908c3798d
merdon-tech <merdon@dhinatechnology.com>
2024-08-13 19:07:18 +0530

    file structure created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import Home from './sections/Home'
+import Meals from './sections/Meals'
+import Gallery from './sections/Gallery'
+import Pricing from './sections/Pricing'
+import 'bootstrap'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <section>
+        <Home />
+      </section>
+      <section>
+        <Meals />
+      </section>
+      <section>
+        <Gallery />
+      </section>
+      <section>
+        <Pricing />
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/sections/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <section className='container-fluid home-bg '>
+        <div className='home-content text-white'> 
+          <p>F<span>OO</span>DTODAY</p>
+          <p>Discover the best Meals & drinks</p>
+          <input type='search' placeholder='Search Foods Drinks here...'/>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Home
